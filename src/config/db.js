02a6057563cf08a209dd9e0b75f3e5450a1c1ed9
Yash <yashkhatri88540@gmail.com>
2026-06-06 20:19:15 +0530

    mongodb error

--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,7 @@ import mongoose  from "mongoose";
 
 
 const connectDB = async ()=> {
+    console.log("MONGO URI:", process.env.MONGODB_URI);
     mongoose.connection.on('connected', ()=>{
         console.log("DB Connected");
     })
