007367f4461158dc5e1a14a9223e2daa34503a03
Yash <yashkhatri88540@gmail.com>
2026-06-06 20:29:37 +0530

    swagger doc fixed

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -11,7 +11,7 @@ const options = {
 
     servers: [
       {
-        url: "hintro-assignment-production-f256.up.railway.app",
+        url: "https://hintro-assignment-production-f256.up.railway.app",
       },
     ],
 
